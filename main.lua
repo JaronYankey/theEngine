78539d33178cbeda4052bdb40755fb40579b23e7
JaronYankey <127556634+JaronYankey@users.noreply.github.com>
2025-11-22 09:45:41 -0500

    Adding audio but got side tracked by the loved 2d library on github

--- main.lua
+++ main.lua
@@ -1,1 +1,8 @@
-print("Hello world")
+function love.load()
+  musicTest = love.audio.newSource("sounds/cave\ themeb4.ogg", "stream")
+  love.audio.play(musicTest)
+
+
+function love.draw()
+	love.graphics.print("Hello, world!", 400, 300)
+end
